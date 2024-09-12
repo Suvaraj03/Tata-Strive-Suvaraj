e253095acaab32b3a390221817ceab0c750521f1
Bharath <Bharath@BHARATH>
2024-09-12 21:46:47 +0530

    Exception Handling

---
+++ src/ExceptionHandling/FindingName.java
@@ -0,0 +1,54 @@
+package ExceptionHandling;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FindingName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the path : ");
+		String path=sc.nextLine();
+		System.out.println("Enter the lucky number : in two digit");
+		int num =sc.nextInt();
+		NameFinder(path,num);
+
+	}
+
+	public static void NameFinder(String path, int num) {
+		// TODO Auto-generated method stub
+		String name = "";
+		String names= "";
+		try(FileReader fr= new FileReader(path);BufferedReader br =new BufferedReader(fr)){
+			while ((name = br.readLine())!=null) {
+				if(num==NameSum(name)) {
+					names +=name +"";
+				}
+			}
+			if(names=="") {
+				System.out.println("There is no matching name with your lucky number");
+			}else {
+				System.out.println(names);
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e);
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private static int NameSum(String name) {
+		int sum =0;
+		for(char ch:name.toLowerCase().toCharArray()) {
+			sum +=ch - 'a' +1;
+			
+		}
+		return sum;
+	}
+
+}
